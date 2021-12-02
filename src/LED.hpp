a5c49836249983d691a6e7846e52accbe4aaad70
Fynn Raddatz <fynn.raddatz@gmail.com>
2021-12-02 04:31:33 +0100

    Added SINE Animation

--- src/LED.hpp
+++ src/LED.hpp
@@ -5,38 +5,32 @@
 
 class LED
 {
-	public:
-		LED(TIM_HandleTypeDef* tim);
-		void setSTAT1(bool on);
-		void setSTAT2(bool on);
-		void setSTAT3(bool on);
-		void toggleSTAT(uint8_t stat);
+public:
+	LED();
+	LED(TIM_HandleTypeDef *tim);
 
-		void updateNeopixels();
+	void updateNeopixels();
 
-		void animateSine(uint8_t* baseColor, float speed, float scaling, float dimming);
+	void animateSine(uint8_t *baseColor, float speed, float scaling, float dimming);
 
-		void setNeopixel(uint8_t* rgb, uint8_t led);
-		void setNeopixel(uint8_t red, uint8_t green, uint8_t blue, uint8_t led);
+	void setNeopixel(uint8_t *rgb, uint8_t led);
+	void setNeopixel(uint8_t red, uint8_t green, uint8_t blue, uint8_t led);
 
-		void setAllNeopixels(uint8_t* rgb);
-		void setAllNeopixels(uint8_t red, uint8_t green, uint8_t blue);
+	void setAllNeopixels(uint8_t *rgb);
+	void setAllNeopixels(uint8_t red, uint8_t green, uint8_t blue);
 
-		uint32_t _updateCNT = 0;
-	private:
-		void setNEOPin(bool on);
-		void toggleNEOPin();
-		void setNeopixelNOUP(uint8_t* rgb, uint8_t led);
-		void setNeopixelNOUP(uint8_t red, uint8_t green, uint8_t blue, uint8_t led);
+	uint32_t _updateCNT = 0;
 
+private:
+	void setNEOPin(bool on);
+	void toggleNEOPin();
+	void setNeopixelNOUP(uint8_t *rgb, uint8_t led);
+	void setNeopixelNOUP(uint8_t red, uint8_t green, uint8_t blue, uint8_t led);
 
+	static const uint8_t _neoLEDS = 8;
 
-		static const uint8_t _neoLEDS = 8;
-
-		uint8_t ledBufferRED[_neoLEDS];
-		uint8_t ledBufferGREEN[_neoLEDS];
-		uint8_t ledBufferBLUE[_neoLEDS];
-		TIM_HandleTypeDef* _tim;
+	uint8_t ledBufferRED[_neoLEDS];
+	uint8_t ledBufferGREEN[_neoLEDS];
+	uint8_t ledBufferBLUE[_neoLEDS];
+	TIM_HandleTypeDef *_tim;
 };
-
-
